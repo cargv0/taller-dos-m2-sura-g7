ad7d75b48bc809047c5a639c23650203682f8ebb
cargv0 <cg34234@gmail.com>
2025-11-21 00:19:21 -0500

    se realiza ejercicio 14

---
+++ ejercicio 14/invertirSinMutar.js
@@ -0,0 +1,6 @@
+let receta = ["Paso 1", "Paso 2", "Paso 3"];
+
+let recetaInversa = [...receta].reverse();
+
+console.log(receta);        // ["Paso 1", "Paso 2", "Paso 3"]
+console.log(recetaInversa); // ["Paso 3", "Paso 2", "Paso 1"]
