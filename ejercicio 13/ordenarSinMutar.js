f8136fd4fb1ea250ff5a8bfdcbd8ca278fe74ab9
cargv0 <cg34234@gmail.com>
2025-11-21 00:18:32 -0500

    se realiza ejercicio 13

---
+++ ejercicio 13/ordenarSinMutar.js
@@ -0,0 +1,6 @@
+let invitados = ["Zapata", "Alvarez", "Gomez", "Bernal"];
+
+let invitadosOrdenados = [...invitados].sort();
+
+console.log(invitados);          // ["Zapata", "Alvarez", "Gomez", "Bernal"]
+console.log(invitadosOrdenados); // ["Alvarez", "Bernal", "Gomez", "Zapata"]
