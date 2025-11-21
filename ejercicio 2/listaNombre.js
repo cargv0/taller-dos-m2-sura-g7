581a6fff8cb90d6cf7de15c775114e99716ce553
cargv0 <cg34234@gmail.com>
2025-11-20 23:41:37 -0500

    se realiza ejercicio 2

---
+++ ejercicio 2/listaNombre.js
@@ -0,0 +1,8 @@
+let usuarios = [
+  {id: 1, nombre: "Ana"},
+  {id: 2, nombre: "Luis"},
+  {id: 3, nombre: "Carlos"}
+];
+
+let nombres = usuarios.map(u => u.nombre);
+console.log(nombres); // ["Ana", "Luis", "Carlos"]
